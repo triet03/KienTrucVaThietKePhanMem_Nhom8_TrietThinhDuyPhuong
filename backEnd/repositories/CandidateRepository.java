d7e4ae691f0d3d78754dd06e4b6ee4b3899cf832
PhuongNgo03112003 <ngoleminhphuong03112003@gmail.com>
2025-04-25 20:40:27 +0700

    update thêm đăng kí đang nhâp có xác thực otp

---
+++ backEnd/repositories/CandidateRepository.java
@@ -0,0 +1,18 @@
+package vn.edu.iuh.fit.backEnd.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import vn.edu.iuh.fit.backEnd.models.Candidate;
+import vn.edu.iuh.fit.backEnd.models.Skill;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CandidateRepository extends JpaRepository<Candidate, Long> {
+	@Query("SELECT c FROM Candidate c JOIN CandidateSkill cs ON c.canId = cs.candidate.canId WHERE cs.skill = :skill")
+	List<Candidate> findCandidatesBySkill(Skill skill);
+
+	Optional<Candidate> findByEmail(String email);
+}
